e60c18a65c94ece964f01a05f9e6e5d928b725b2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-08-03 15:16:08 +0300

    Fixed the style of server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import controllerRouting from './routes/index';
 
 const app = express();
+// eslint-disable-next-line
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
